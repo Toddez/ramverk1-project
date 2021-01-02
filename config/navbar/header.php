66b2af9e50b382c90fa9c7f3306c855852d0b534
Teo Carlsson <carlsson.teo@gmail.com>
2021-01-02 14:58:35 +0100

    Added navbar items

---
+++ config/navbar/header.php
@@ -0,0 +1,19 @@
+<?php
+
+return [
+    "wrapper" => null,
+    "class" => "my-navbar rm-default rm-desktop",
+ 
+    "items" => [
+        [
+            "text" => "Hem",
+            "url" => "",
+            "title" => "Start sidan.",
+        ],
+        [
+            "text" => "Om",
+            "url" => "om",
+            "title" => "Om denna webbplats.",
+        ],
+    ],
+];
