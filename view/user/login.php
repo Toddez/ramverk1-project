732ed029b887273942b479835fa50f8c55bcbf10
Teo Carlsson <carlsson.teo@gmail.com>
2021-01-02 18:05:03 +0100

    Added views for login and register

---
+++ view/user/login.php
@@ -0,0 +1,9 @@
+<?php
+
+?>
+
+<div class="login-wrap">
+    <?= $form ?>
+
+    <p>Har du inget konto? <a href="register">Skapa ett konto.</a></p>
+</div>
---
+++ view/user/register.php
@@ -0,0 +1,9 @@
+<?php
+
+?>
+
+<div class="login-wrap">
+    <?= $form ?>
+
+    <p>Har du redan ett konto? <a href="login">Logga in här.</a></p>
+</div>
