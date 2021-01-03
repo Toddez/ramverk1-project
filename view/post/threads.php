e01fa59242a57cbd3ff0a08b16b1d67081a3335f
Teo Carlsson <carlsson.teo@gmail.com>
2021-01-03 16:46:54 +0100

    Added view to display threads

---
+++ view/post/threads.php
@@ -0,0 +1,34 @@
+<?php
+
+?>
+
+<a href="threads/new">Ny fråga</a>
+
+<div class="threads">
+    <div class="thread">
+        <div class="left">
+            <div class="score">
+                <span class="number">570</span>
+                <span>röster</span>
+            </div>
+            <div class="answers">
+                <span class="number">12</span>
+                <span>svar</span>
+            </div>
+        </div>
+        <div class="right">
+            <div class="content">
+                <a class="title">Q: Hur gör man x?</a>
+                <div class="snippet">Jag försökte göra y, men jag stötte på ett problem då...</div>
+            </div>
+            <div class="details">
+                <div class="tags">
+                    <a class="tag">Första</a>
+                    <a class="tag">Enkel</a>
+                </div>
+                <div class="date">Jan 1 '21</div>
+                <a class="name">User</a>
+            </div>
+        </div>
+    </div>
+</div>
