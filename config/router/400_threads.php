1d0dace94f3fe34e4da1279c686acb625902356e
Teo Carlsson <carlsson.teo@gmail.com>
2021-01-03 16:47:15 +0100

    Added router for threads

---
+++ config/router/400_threads.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Routes for controller.
+ */
+return [
+    "routes" => [
+        [
+            "info" => "Controller for threads.",
+            "mount" => "threads",
+            "handler" => "\Teca\Post\ThreadController",
+        ],
+    ]
+];
