09e851e82fe2dd4fefdf9b1348da2a28309cca14
Teo Carlsson <carlsson.teo@gmail.com>
2021-01-03 17:32:02 +0100

    Fetch threads from database to then display

--- src/Post/ThreadController.php
+++ src/Post/ThreadController.php
@@ -6,6 +6,8 @@ use Anax\Commons\ContainerInjectableInterface;
 use Anax\Commons\ContainerInjectableTrait;
 use Teca\Post\HTMLForm\NewThreadForm;
 use Teca\User\User;
+use Teca\Post\Post;
+use Teca\Post\PostType;
 
 class ThreadController implements ContainerInjectableInterface
 {
@@ -15,7 +17,15 @@ class ThreadController implements ContainerInjectableInterface
     {
         $page = $this->di->get("page");
 
+        $post = new Post();
+        $post->setDb($this->di->get("dbqb"));
+        $threads = $post->findAllWhere("type = ?", PostType::THREAD);
+
+        $date = array_column($threads, 'creation');
+        array_multisort($date, SORT_ASC, $threads);
+
         $page->add("post/threads", [
+            "threads" => $threads,
         ]);
 
         return $page->render([
