cb8163acdadcdeb4fc642ff91ad02905b5a02f97
Teo Carlsson <carlsson.teo@gmail.com>
2021-01-03 16:46:40 +0100

    Changed title of profile

--- src/User/UserController.php
+++ src/User/UserController.php
@@ -29,7 +29,7 @@ class UserController implements ContainerInjectableInterface
         ]);
 
         return $page->render([
-            "title" => "Logga in",
+            "title" => "Profil",
         ]);
     }
 
