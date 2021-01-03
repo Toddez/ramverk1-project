6ef11e88a5c28c90039144d3e2e49a09a5220c61
Teo Carlsson <carlsson.teo@gmail.com>
2021-01-03 15:27:21 +0100

    Added Profile form

---
+++ src/User/HTMLForm/ProfileForm.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace Teca\User\HTMLForm;
+
+use Anax\HTMLForm\FormModel;
+use Psr\Container\ContainerInterface;
+use Teca\User\User;
+
+class ProfileForm extends FormModel
+{
+    public function __construct(ContainerInterface $di)
+    {
+        $user = User::currentUser($di);
+
+        parent::__construct($di);
+        $this->form->create(
+            [
+                "id" => "ProfileForm",
+                "legend" => "Profil",
+            ],
+            [
+                "avatar" => [
+                    "type" => "text",
+                    "label" => "Avatar (hämtas från gravatar)",
+                    "value" => $user->avatar
+                ],
+                "submit" => [
+                    "type" => "submit",
+                    "value" => "Spara ändringar",
+                    "callback" => [$this, "callbackSubmit"]
+                ],
+            ]
+        );
+    }
+
+    public function callbackSubmit() : bool
+    {
+        $avatar = $this->form->rawValue("avatar");
+
+        $user = User::currentUser($this->di);
+        $user->avatar = $avatar;
+        $user->save();
+
+        return true;
+    }
+
+    public function callbackSuccess()
+    {
+        $this->di->get("response")->redirectSelf();
+    }
+}
