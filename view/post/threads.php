981ffd60de3b56ccf4e22506a55d585d00d2d428
Teo Carlsson <carlsson.teo@gmail.com>
2021-01-03 17:43:27 +0100

    Display authors name instead of id

--- view/post/threads.php
+++ view/post/threads.php
@@ -30,7 +30,7 @@ if ($authorized) { ?>
                     <a class="tag">tmp</a>
                 </div>
                 <div class="date"><?= date("H:i F j 'y", $thread->creation) ?></div>
-                <a class="name" href="user/view/<?= $thread->author ?>"><?= htmlentities($thread->author) ?></a>
+                <a class="name" href="user/view/<?= $thread->author ?>"><?= htmlentities($thread->authorName) ?></a>
             </div>
         </div>
     </div>
