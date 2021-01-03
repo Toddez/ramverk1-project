e07bae786094063660f7bea320f6e73c2eeca19f
Teo Carlsson <carlsson.teo@gmail.com>
2021-01-03 17:31:03 +0100

    Added logic for creating thread

--- src/Post/HTMLForm/NewThreadForm.php
+++ src/Post/HTMLForm/NewThreadForm.php
@@ -4,10 +4,14 @@ namespace Teca\Post\HTMLForm;
 
 use Anax\HTMLForm\FormModel;
 use Psr\Container\ContainerInterface;
+use Teca\User\User;
 use Teca\Post\Post;
+use Teca\Post\PostType;
 
 class NewThreadForm extends FormModel
 {
+    private $id;
+
     public function __construct(ContainerInterface $di)
     {
         parent::__construct($di);
@@ -20,12 +24,16 @@ class NewThreadForm extends FormModel
                 "title" => [
                     "type" => "text",
                     "label" => "Titel",
-                    "validation" => [],
+                    "validation" => [
+                        "not_empty"
+                    ],
                 ],
                 "content" => [
                     "type" => "textarea",
                     "label" => "Fråga",
-                    "validation" => [],
+                    "validation" => [
+                        "not_empty"
+                    ],
                 ],
                 "submit" => [
                     "type" => "submit",
@@ -38,11 +46,32 @@ class NewThreadForm extends FormModel
 
     public function callbackSubmit() : bool
     {
-        return false;
+        $title = $this->form->value("title");
+        $content = $this->form->value("content");
+
+        $user = new User();
+        $user->currentUser($this->di);
+
+        if (!$user->id) {
+            return false;
+        }
+
+        $post = new Post();
+        $post->setDb($this->di->get("dbqb"));
+        $post->author = $user->id;
+        $post->title = $title;
+        $post->content = $content;
+        $post->type = PostType::THREAD;
+        $post->creation = time();
+        $post->save();
+
+        $this->id = $post->id;
+
+        return true;
     }
 
     public function callbackSuccess()
     {
-        // TODO: Redirect to thread
+        $this->di->get("response")->redirect("thread/view/" . $this->id);
     }
 }
