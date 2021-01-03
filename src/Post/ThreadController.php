bb87ae8576a03d8efcd5ad1fc300c00d727647fd
Teo Carlsson <carlsson.teo@gmail.com>
2021-01-03 16:52:51 +0100

    Added route for new thread

--- src/Post/ThreadController.php
+++ src/Post/ThreadController.php
@@ -4,6 +4,8 @@ namespace Teca\Post;
 
 use Anax\Commons\ContainerInjectableInterface;
 use Anax\Commons\ContainerInjectableTrait;
+use Teca\Post\HTMLForm\NewThreadForm;
+use Teca\User\User;
 
 class ThreadController implements ContainerInjectableInterface
 {
@@ -17,7 +19,27 @@ class ThreadController implements ContainerInjectableInterface
         ]);
 
         return $page->render([
-            "title" => "Samtliga Frågor",
+            "title" => "Samtliga frågor",
+        ]);
+    }
+
+    public function newAction() : object
+    {
+        $user = new User();
+        if (!$user->authorized($this->di)) {
+            $this->di->get("response")->redirect("user/login")->send();
+        }
+
+        $page = $this->di->get("page");
+        $form = new NewThreadForm($this->di);
+        $form->check();
+
+        $page->add("post/newthread", [
+            "form" => $form->getHTML(),
+        ]);
+
+        return $page->render([
+            "title" => "Ny fråga",
         ]);
     }
 }
