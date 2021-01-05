0a3b4e1580e66b7cd7d70b2184c5409e965d0eaf
Teo Carlsson <carlsson.teo@gmail.com>
2021-01-05 12:55:30 +0100

    Added logic to view all threads of specific type

--- src/Tag/TagController.php
+++ src/Tag/TagController.php
@@ -7,6 +7,8 @@ use Anax\Commons\ContainerInjectableTrait;
 use Teca\Post\Post;
 use Teca\Post\PostType;
 use Teca\Tag\Tag;
+use Teca\Vote\Vote;
+use Teca\User\User;
 
 class TagController implements ContainerInjectableInterface
 {
@@ -50,8 +52,79 @@ class TagController implements ContainerInjectableInterface
     {
         $page = $this->di->get("page");
 
-        $page->add("tag/threads", [
+        $tag = new Tag();
+        $tag->setDb($this->di->get("dbqb"));
+        $targetTag = $tag->findWhere("id = ?", $tagId);
 
+        $post = new Post();
+        $post->setDb($this->di->get("dbqb"));
+        $posts = $post->findAll();
+
+        $threads = [];
+        foreach ($posts as $post) {
+            if (in_array($targetTag->id, explode(",", $post->tags))) {
+                $threads[] = $post;
+            }
+        }
+
+        $threadIds = array_column($posts, "id");
+        $tagIds = array_unique(explode(",", implode(",", array_column($threads, "tags"))));
+
+        $vote = new Vote();
+        $vote->setDb($this->di->get("dbqb"));
+        $votes = $vote->findAllWhere("post IN (?)", [$threadIds]);
+
+        $authors = array_unique(array_column($threads, 'author'));
+        $user = new user();
+        $user->setDb($this->di->get("dbqb"));
+        $users = $user->findAllWhere("id IN (?)", [$authors]);
+
+        $post = new Post();
+        $post->setDb($this->di->get("dbqb"));
+        $answers = $post->findAllWhere("thread IN (?) AND type = ?", [$threadIds, PostType::ANSWER]);
+
+        $tag = new Tag();
+        $tag->setDb($this->di->get("dbqb"));
+        $tags = $tag->findAllWhere("id IN (?)", [$tagIds]);
+
+        foreach ($threads as $thread) {
+            $id = $thread->author;
+            $thread->answerCount = 0;
+            $thread->voteCount = 0;
+            $thread->tagValues = [];
+            foreach ($users as $author) {
+                if ($author->id === $id) {
+                    $thread->authorName = $author->name;
+                    $thread->authorAvatar = $author->gravatar();
+                }
+            }
+
+            foreach ($answers as $answer) {
+                if (intval($answer->thread) === intval($thread->id)) {
+                    $thread->answerCount++;
+                }
+            }
+
+            foreach ($votes as $vote) {
+                if (intval($vote->post) === intval($thread->id)) {
+                    $thread->voteCount += intval($vote->value);
+                }
+            }
+
+            foreach ($tags as $tag) {
+                if (in_array($tag->id, explode(",", $thread->tags))) {
+                    $thread->tagValues[] = $tag;
+                }
+            }
+        }
+
+        $creationDates = array_column($posts, 'creation');
+        array_multisort($creationDates, SORT_DESC, $posts);
+
+        $page->add("post/threads", [
+            "threads" => $threads,
+            "text" => "Alla frågor med taggen: " . $targetTag->value,
+            "prefix" => "../../",
         ]);
 
         return $page->render([
