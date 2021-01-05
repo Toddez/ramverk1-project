44a5bde2860385d0048bf5bdf5aa0570d6383d4b
Teo Carlsson <carlsson.teo@gmail.com>
2021-01-05 13:54:53 +0100

    Added router config for index

---
+++ config/router/0_index.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+    "routes" => [
+        [
+            "info" => "Controller for index.",
+            "mount" => "",
+            "path" => "",
+            "handler" => "\Teca\Index\IndexController",
+        ],
+    ]
+];
