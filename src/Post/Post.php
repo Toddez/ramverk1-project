fd3e0998a78704dbdfabdd3a50dbb2d94a3f011f
Teo Carlsson <carlsson.teo@gmail.com>
2021-01-09 14:41:55 +0100

    Added support for score and marked answer to Post model

--- src/Post/Post.php
+++ src/Post/Post.php
@@ -3,6 +3,8 @@
 namespace Teca\Post;
 
 use Anax\DatabaseActiveRecord\ActiveRecordModel;
+use Teca\Post\PostType;
+use Teca\Vote\Vote;
 
 class Post extends ActiveRecordModel
 {
@@ -16,4 +18,32 @@ class Post extends ActiveRecordModel
     public $thread;
     public $parent;
     public $creation;
+    public $answer;
+
+    public function score($di) : int
+    {
+        $vote = new Vote();
+        $vote->setDb($di->get("dbqb"));
+        $votes = $vote->findAllWhere("post = ?", [$this->id]);
+
+        $sum = 0;
+        foreach ($votes as $vote) {
+            $sum += $vote->value;
+        }
+
+        return $sum;
+    }
+
+    public function hasAnswer($di) : bool
+    {
+        $post = new Post();
+        $post->setDb($di->get("dbqb"));
+        $post = $post->findWhere("thread = ? AND type = ? AND answer = ?", [$this->id, PostType::ANSWER, true]);
+
+        if ($post->id) {
+            return true;
+        }
+
+        return false;
+    }
 }
